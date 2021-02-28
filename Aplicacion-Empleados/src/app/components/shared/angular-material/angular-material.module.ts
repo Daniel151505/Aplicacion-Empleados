55a0fab120a499e8799f37515081adf8ab065396
Daniel151505 <daniel150520011@gmail.com>
2021-02-27 20:38:00 -0500

    Importando MatTableModule

--- Aplicacion-Empleados/src/app/components/shared/angular-material/angular-material.module.ts
+++ Aplicacion-Empleados/src/app/components/shared/angular-material/angular-material.module.ts
@@ -5,17 +5,20 @@ import { CommonModule } from '@angular/common';
 
 import { MatSliderModule } from '@angular/material/slider';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSliderModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule
   ],
   exports: [
     MatSliderModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule
   ]
 })
 export class AngularMaterialModule { }
