4dd50cc4824abfd8124d57429d56644c4d1b5b29
Daniel151505 <daniel150520011@gmail.com>
2021-02-27 22:49:00 -0500

    Creando Objeto

--- Aplicacion-Empleados/src/app/services/empleado.service.ts
+++ Aplicacion-Empleados/src/app/services/empleado.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { EmpleadoModule } from '../models/empleado/empleado.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpleadoService {
+  listaEmpleado: EmpleadoModule[] = [
+    {
+      nombreCompleto: 'Lucas Martinez', 
+      correo: 'martinez@gmail.com',
+      telefono: 12453274,
+      sexo: 'Masculino',
+      fechaIngreso: new Date(),
+      estadoCivil: 'Soltero'
+    }
+  ]
 
   constructor() { }
 }
