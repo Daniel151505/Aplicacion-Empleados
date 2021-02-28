97bea20cb8cabcc3e019a8bdb3a1a4ea68e5c03e
Daniel151505 <daniel150520011@gmail.com>
2021-02-27 20:51:25 -0500

    Importando MatFormFieldModule

--- Aplicacion-Empleados/src/app/components/shared/angular-material/angular-material.module.ts
+++ Aplicacion-Empleados/src/app/components/shared/angular-material/angular-material.module.ts
@@ -6,19 +6,21 @@ import { CommonModule } from '@angular/common';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSliderModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule
   ],
   exports: [
     MatSliderModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule
   ]
 })
 export class AngularMaterialModule { }
