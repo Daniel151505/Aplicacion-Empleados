b46c9a78ea233367973882c203176899575ae164
Daniel151505 <daniel150520011@gmail.com>
2021-02-27 21:18:45 -0500

    Importando MatPaginator

--- Aplicacion-Empleados/src/app/components/listar-empleado/listar-empleado.component.ts
+++ Aplicacion-Empleados/src/app/components/listar-empleado/listar-empleado.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-
+import {MatPaginator} from '@angular/material/paginator';
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -30,9 +30,11 @@ export class ListarEmpleadoComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource.paginator = this.paginator;
   }
 
   applyFilter(event: Event) {
