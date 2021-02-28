ba99dc53746fd9522788bfdbf79d51b6e4cf4aea
Daniel151505 <daniel150520011@gmail.com>
2021-02-27 20:57:57 -0500

    Agregando Metodo Apply Filter

--- Aplicacion-Empleados/src/app/components/listar-empleado/listar-empleado.component.ts
+++ Aplicacion-Empleados/src/app/components/listar-empleado/listar-empleado.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-listar-empleado',
   templateUrl: './listar-empleado.component.html',
   styleUrls: ['./listar-empleado.component.css']
 })
 export class ListarEmpleadoComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
