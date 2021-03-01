5c850dfac6a84ff6fcb127f9c3be117178d08255
Daniel151505 <daniel150520011@gmail.com>
2021-02-28 19:07:01 -0500

    Agregando Objeto

--- Aplicacion-Empleados/src/app/services/empleado.service.ts
+++ Aplicacion-Empleados/src/app/services/empleado.service.ts
@@ -7,12 +7,32 @@ import { Empleado } from '../models/empleado.module';
 export class EmpleadoService {
   listaEmpleado: Empleado[] = [
     {
-      nombreCompleto: 'Lucas Martinez', 
-      correo: 'martinez@gmail.com',
-      telefono: 12453274,
-      sexo: 'Masculino',
-      fechaIngreso: new Date(),
-      estadoCivil: 'Soltero'
+      nombreCompleto: 'Lucas Martinez', correo: 'lmartinez@gmail.com', telefono: 3512335522,
+      sexo: 'Masculino', fechaIngreso: new Date(), estadoCivil: 'Soltero'
+    },
+    {
+      nombreCompleto: 'Rodrigo Aliaga', correo: 'raliaga@gmail.com',
+      estadoCivil: 'Soltero', fechaIngreso: new Date('2019-05-25'), sexo: 'Masculino', telefono: 3512334422
+    },
+    {
+      nombreCompleto: 'Maria Funes', correo: 'mfunes@gmail.com',
+      estadoCivil: 'Casado', fechaIngreso: new Date('2020-04-27'), sexo: 'Femenino', telefono: 3512553322
+    },
+    {
+      nombreCompleto: 'Lucrecia Juarez', correo: 'maria@gmail.com',
+      estadoCivil: 'Soltero', fechaIngreso: new Date('2020-07-25'), sexo: 'Femenino', telefono: 3514665533
+    },
+    {
+      nombreCompleto: 'Federico Gonzalez', correo: 'fgonzalez@gmail.com',
+      estadoCivil: 'Soltero', fechaIngreso: new Date('2020-02-31'), sexo: 'Femenino', telefono: 1156688332
+    },
+    {
+      nombreCompleto: 'Estefania Schutz', correo: 'eschutz@gmail.com',
+      estadoCivil: 'Soltero', fechaIngreso: new Date('2020-01-31'), sexo: 'Femenino', telefono: 1156678679
+    },
+    {
+      nombreCompleto: 'Maria Belen Arzu', correo: 'mbarzu@gmail.com',
+      estadoCivil: 'Soltero', fechaIngreso: new Date('2020-01-31'), sexo: 'Femenino', telefono: 3512576589
     }
   ]
 
