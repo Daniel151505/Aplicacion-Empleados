50577bcbfbc0cb1c50a108f5730454ba9c650244
Daniel151505 <daniel150520011@gmail.com>
2021-02-28 19:07:24 -0500

    Fix error

--- Aplicacion-Empleados/src/app/components/listar-empleado/listar-empleado.component.ts
+++ Aplicacion-Empleados/src/app/components/listar-empleado/listar-empleado.component.ts
@@ -4,25 +4,6 @@ import {MatPaginator} from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { EmpleadoService } from 'src/app/services/empleado.service';
 import { Empleado } from 'src/app/models/empleado.module';
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-listar-empleado',
@@ -30,17 +11,16 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./listar-empleado.component.css']
 })
 export class ListarEmpleadoComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
-  listaEmpleado : Empleado []
+  displayedColumns: string[] = ['nombreCompleto', 'correo', 'estadoCivil', 'fechaIngreso', 'sexo', 'telefono', 'acciones'];
+  dataSource = new MatTableDataSource();
+  listaEmpleado : Empleado[]
 
-  @ViewChild(MatSort , {static: true}) sort: MatSort;
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   constructor(private empleadoService: EmpleadoService) { }
 
   ngOnInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    this.cargarEmpleados();
   }
 
   applyFilter(event: Event) {
@@ -50,6 +30,9 @@ export class ListarEmpleadoComponent implements OnInit {
 
   cargarEmpleados(){
     this.listaEmpleado = this.empleadoService.getEmpleado()
+    this.dataSource = new MatTableDataSource(this.listaEmpleado);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
 }
